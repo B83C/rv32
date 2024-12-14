3e792968924ea49c463aa5cbcb319ea80bcc4c58
B83C <b83c@outlook.com>
2024-12-15 00:03:34 +0800

    Some fixes

--- src/pipeline_unit.sv
+++ src/pipeline_unit.sv
@@ -18,52 +18,45 @@ module pipeline_unit (
   wire [31:0] jmp_addr;
  
   wire [31:0] instr_d, pc, pc_f, pc_d, pc_e, pc_m, pc_w;
-  rbuffer #(32) instr_bd (clk, !stall_d, rst_n, instr, instr_d);
+  rbuffer #($size(instr)) instr_bd (clk, !stall_d, rst_n, instr, instr_d);
+  rbuffer #($size(pc), 5) pc_b (clk, {!stall_f, !stall_d, 3'b111}, {5{rst_n}}, pc, {pc_f, pc_d, pc_e, pc_m, pc_w});
 
-  rbuffer #(32) pc_bf (clk, !stall_f, rst_n, pc, pc_f);
-  rbuffer #(32) pc_bd (clk, !stall_d, rst_n, pc_f, pc_d);
-  rbuffer #(32) pc_be (clk, 1, rst_n, pc_d, pc_e);
-  rbuffer #(32) pc_bm (clk, 1, rst_n, pc_e, pc_m);
-  rbuffer #(32) pc_bw (clk, 1, rst_n, pc_m, pc_w);
-
-  wire register_data_sel r1_sel, r2_sel;
+  register_data_sel r1_sel, r2_sel;
   wire [4:0] rs1, rs1_e;
   wire [4:0] rs2, rs2_e;
-  rbuffer #(5) rs1_be (clk, 1, rst_n, rs1, rs1_e);
-  rbuffer #(5) rs2_be (clk, 1, rst_n, rs2, rs2_e);
+  rbuffer #($size(rs1)) rs1_be (clk, 1, rst_n, rs1, rs1_e);
+  rbuffer #($size(rs2)) rs2_be (clk, 1, rst_n, rs2, rs2_e);
+
   wire [4:0] rd, rd_e, rd_m, rd_w;
-  rbuffer #(5) rd_be (clk, 1, rst_n, rd, rd_e);
-  rbuffer #(5) rd_bm (clk, 1, rst_n, rd_e, rd_m);
-  rbuffer #(5) rd_bw (clk, 1, rst_n, rd_m, rd_w);
+  rbuffer #($size(rd), 3) rd_b (clk, 3'b111, {3{rst_n}}, rd, {rd_e, rd_m, rd_w});
 
   wire [31:0] r1, r1_e;
-  rbuffer #(32) r1_be (clk, 1, rst_n, r1, r1_e);
+  rbuffer #($size(r1)) r1_be (clk, 1, rst_n, r1, r1_e);
   wire [31:0] r2, r2_e;
-  rbuffer #(32) r2_be (clk, 1, rst_n, r2, r2_e);
+  rbuffer #($size(r2)) r2_be (clk, 1, rst_n, r2, r2_e);
 
   wire [31:0] r1_mux, r2_mux, r2_mux_m;
-  rbuffer #(32) r2_mux_bm (clk, 1, rst_n, r2_mux, r2_mux_m);
+  rbuffer #($size(r2_mux)) r2_mux_bm (clk, 1, rst_n, r2_mux, r2_mux_m);
 
   wire [31:0] imm, imm_e;
-  rbuffer #(32) imm_be (clk, 1, rst_n, imm, imm_e);
+  rbuffer #($size(imm)) imm_be (clk, 1, rst_n, imm, imm_e);
 
   wire [2:0] func3, func3_e;
-  rbuffer #(3) func3_be (clk, 1, rst_n, func3, func3_e);
+  rbuffer #($size(func3)) func3_be (clk, 1, rst_n, func3, func3_e);
   wire [1:0] alu_src_sel, alu_src_sel_e;
-  rbuffer #(2) alu_src_sel_be (clk, 1, rst_n, alu_src_sel, alu_src_sel_e);
+  rbuffer #($size(alu_src_sel)) alu_src_sel_be (clk, 1, rst_n, alu_src_sel, alu_src_sel_e);
 
-  wire control_signals_t cs, cs_e, cs_m, cs_w;
-  rbuffer #($size(control_signals_t)) cs_be (clk, 1, rst_n, cs, cs_e);
-  rbuffer #($size(control_signals_t)) cs_bm (clk, 1, rst_n, cs_e, cs_m);
-  rbuffer #($size(control_signals_t)) cs_bw (clk, 1, rst_n, cs_m, cs_w);
+  control_signals_t cs, cs_e, cs_m, cs_w;
+  rbuffer #($size(control_signals_t), 3) cs_b (clk, 3'b111, {3{rst_n}}, cs, {cs_e, cs_m, cs_w});
 
   wire [31:0] alu_mux_input_1;
   wire [31:0] alu_mux_input_2;
   wire [31:0] alu_res, alu_res_m, alu_res_w;
-  rbuffer #(32) alu_res_bm (clk, 1, rst_n, alu_res, alu_res_m);
-  rbuffer #(32) alu_res_bw (clk, 1, rst_n, alu_res_m, alu_res_w);
+  rbuffer #($size(alu_res)) alu_res_bm (clk, 1, rst_n, alu_res, alu_res_m);
+  // rbuffer #($size(alu_res)) alu_res_bw (clk, 1, rst_n, alu_res_m, alu_res_w);
+
   wire [31:0] mul_res;
-  rbuffer #(32) mul_res_bw (clk, 1, rst_n, (cs_w.m)? mul_res: alu_res_m, alu_res_w);
+  rbuffer #($size(mul_res)) mul_res_bw (clk, 1, rst_n, (cs_w.m)? mul_res: alu_res_m, alu_res_w);
 
   wire [31:0] write_back;
 
@@ -74,7 +67,7 @@ module pipeline_unit (
 
   //Stage 2
 
-  ir_dec_ctrl ir_dec (.ir(instr), .cs(cs), .rs1(rs1), .rs2(rs2), .rd(rd), .imm(imm),.func3(func3), .alu_src_sel(alu_src_sel));
+  ir_dec_ctrl ir_dec (.ir(instr_d), .cs(cs), .rs1(rs1), .rs2(rs2), .rd(rd), .imm(imm),.func3(func3), .alu_src_sel(alu_src_sel));
 
   registers reg_file (
       .clk(clk),
@@ -93,9 +86,9 @@ module pipeline_unit (
   assign jmp_addr = pc_e + imm_e;
 
   branch branch_e (
-    .en(cs.b),
-    .opr1(r1_e),
-    .opr2(r2_e),
+    .en(cs_e.b),
+    .opr1(alu_mux_input_1),
+    .opr2(alu_mux_input_2),
     .func3(func3_e),
     .success(branch_hit)
     );
@@ -111,7 +104,7 @@ module pipeline_unit (
   assign r1_mux = reg_mux(r1_e, r1_sel);
   assign r2_mux = reg_mux(r2_e, r2_sel);
 
-  assign alu_mux_input_1 = (cs.ignore_first_operand)? 0: alu_src_sel_e[0]? pc_e: r1_mux;
+  assign alu_mux_input_1 = (cs_e.ignore_first_operand)? 0: alu_src_sel_e[0]? pc_e: r1_mux;
   assign alu_mux_input_2 = alu_src_sel_e[1]? imm_e: r2_mux;
 
   alu alu_e (
--- src/rbuffer.sv
+++ src/rbuffer.sv
@@ -1,17 +1,31 @@
 `timescale 1ns / 1ps
 
-module rbuffer #(parameter Nbits = 32 )(
+module rbuffer #(parameter Nbits = 32, parameter RLen= 1)(
     input clk,
-    input en,
-    input rst_n,
+    input [RLen-1:0] en,
+    input [RLen-1:0] rst_n,
     input [Nbits-1:0] in,
-    output logic [Nbits-1:0] out
+    output logic [RLen - 1:0][Nbits-1:0] out
 );
   always @(posedge clk, negedge rst_n) begin
-    if(!rst_n) begin
-      out <= 0;
-    end else if (en) begin
-      out <= in;
+    if(!rst_n[RLen - 1]) begin
+      out[RLen - 1] <= 0;
+    end else if (en[RLen - 1]) begin
+        out[RLen - 1] <= in;
     end
   end
+
+  genvar i;
+  generate
+    for(i = RLen - 2; i >= 0; i--) begin
+      always @(posedge clk, negedge rst_n) begin
+        if(!rst_n[i]) begin
+          out[i] <= 0;
+        end else if (en[i]) begin
+            out[i] <= out[i+1];
+        end
+      end
+    end
+  endgenerate
+
 endmodule
