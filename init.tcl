90321fdd9fd42c8db1dfe957b82b84a26fa0ee10
B83C <b83c@outlook.com>
2024-12-20 00:22:58 +0800

    Fixed build.tcl

--- init.tcl
+++ init.tcl
@@ -1,5 +1,5 @@
-set_param general.maxThreads 16
-set partNum xc7a35tcpg236-1
+set_param general.maxThreads 8
+# set partNum xc7a35tcpg236-1
 set_part xc7a35tcpg236-1
 set script_dir [file dirname [file normalize [info script]]]
 
@@ -11,15 +11,7 @@ set checkpoints_dir $working_dir/checkpoints
 set src_dir $root_dir/src
 set ips_dir $root_dir/ips
 
-if {![file isdirectory $working_dir]} {
-    file mkdir $working_dir
-}
-if {![file isdirectory $output_dir]} {
-    file mkdir $output_dir
-}
-if {![file isdirectory $report_dir]} {
-    file mkdir $report_dir
-}
-if {![file isdirectory $checkpoints_dir]} {
-    file mkdir $checkpoints_dir
-}
+file mkdir $working_dir
+file mkdir $output_dir
+file mkdir $report_dir
+file mkdir $checkpoints_dir
