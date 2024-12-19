4b87ccc0fa1bc9970e99759438115fac8d13d7cb
B83C <b83c@outlook.com>
2024-12-19 22:56:52 +0800

    Fixed build.tcl

--- init.tcl
+++ init.tcl
@@ -1,14 +1,12 @@
 set_param general.maxThreads 16
 set partNum xc7a35tcpg236-1
 set_part xc7a35tcpg236-1
-set root_dir [file normalize .]
+set script_dir [file dirname [file normalize [info script]]]
+
+set root_dir $script_dir
 set working_dir $root_dir/target
 set output_dir $working_dir/out
 set report_dir $working_dir/report
 set checkpoints_dir $working_dir/checkpoints
 set src_dir $root_dir/src
 set ips_dir $root_dir/ips
-
-set script_dir [file dirname [file normalize [info script]]]
-cd $script_dir
-puts $script_dir
