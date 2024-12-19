94ff89d5502ad45d86740757b030938ebc3f8e3a
B83C <b83c@outlook.com>
2024-12-19 22:59:32 +0800

    Fixed build.tcl

--- init.tcl
+++ init.tcl
@@ -10,3 +10,16 @@ set report_dir $working_dir/report
 set checkpoints_dir $working_dir/checkpoints
 set src_dir $root_dir/src
 set ips_dir $root_dir/ips
+
+if {![file isdirectory $working_dir]} {
+    file mkdir $working_dir
+}
+if {![file isdirectory $output_dir]} {
+    file mkdir $output_dir
+}
+if {![file isdirectory $report_dir]} {
+    file mkdir $report_dir
+}
+if {![file isdirectory $checkpoints_dir]} {
+    file mkdir $checkpoints_dir
+}
